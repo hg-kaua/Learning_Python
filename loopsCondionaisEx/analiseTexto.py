096c43b21c8fe51c86bb84c3495a649fae1ba280
Hugo Kaua <hugokaua2002@gmail.com>
2023-08-13 18:53:24 -0300

    commit

--- loopsCondionaisEx/analiseTexto.py
+++ loopsCondionaisEx/analiseTexto.py
@@ -4,13 +4,14 @@
 # Conte quantas vezes cada palavra aparece no texto e exiba as palavras mais frequentes.
 # Calcule a média de comprimento das palavras no texto.
 
-def get_texto(text):
+def get_text(text):
     contador_letras(text)
     palavra_frequente(text)
+    media_comprimento(text)
 
 def contador_letras(text):
     count = 0
-    for char in texto:
+    for char in text:
         if char.isalpha():
             count += 1
     return count
@@ -34,7 +35,16 @@ def palavra_frequente(text):
 # TERMINAR
 def media_comprimento(text):
     word_text = text.lower()
+    word_list = word_text.split(" ")
     
-    for char in word_text: 
-        taman
+    total_char = sum(len(word) for word in word_list)
+    total_words = len(word_list)
     
+    average = total_char / total_words
+    
+    print(f"A media do comprimento das palavras é de {average:.0f} caracteres")
+
+    
+
+text_sample = "Python is a versatile programming language that is widely used for various purposes such as web development data analysis artificial intelligence and more"
+get_text(text_sample)
