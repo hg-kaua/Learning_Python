eea225c64e3ba24bafc4e22ac5a5555320393a8f
Hugo Kaua <hugokaua2002@gmail.com>
2023-08-14 23:21:30 -0300

    Update CalculadoraImc.py

--- Projetos !!!!!!/CalculadoraImc.py
+++ Projetos !!!!!!/CalculadoraImc.py
@@ -46,4 +46,29 @@ app_nome.place(x=0, y=2)
 app_linha = Label(frame_cima, text="", width=400, height=1, padx=0, relief="flat", anchor="nw", font=('Arial 1'), bg=co2, fg=co1)
 app_linha.place(x=0, y=35)
 
+# Configurando frame baixo
+
+l_peso = Label(frame_baixo , text="Insira seu peso", height=1, padx=0, relief="flat", anchor="center", font=('Ivy 10 bold'), bg=co1, fg=co0)
+l_peso.grid(row=0, column=0, columnspan=1,  sticky=NW, pady=10, padx=3)
+e_peso = Entry(frame_baixo, width=5, font=('Ivy 10 bold'),justify='center',relief=SOLID)
+e_peso.grid(row=0, column=1, columnspan=1,  sticky=NSEW, pady=10, padx=3)
+ 
+l_altura = Label(frame_baixo , text="Insira sua altura", height=1, padx=0, relief="flat", anchor="center", font=('Ivy 10 bold'), bg=co1, fg=co0)
+l_altura.grid(row=1, column=0, columnspan=1,  sticky=NW, pady=10, padx=3)
+e_altura = Entry(frame_baixo, width=5, font=('Ivy 10 bold'),justify='center',relief=SOLID)
+e_altura.grid(row=1, column=1, columnspan=1,  sticky=NSEW, pady=10, padx=3)
+ 
+ 
+l_resultado = Label(frame_baixo ,width=5, text="---", height=1, padx=6, pady=12, relief="flat", anchor="center", font=('Ivy 24 bold'), bg=co2, fg=co1)
+l_resultado.place(x=175, y=10)
+ 
+l_resultado_texto = Label(frame_baixo , width=37, text="", height=1, padx=0, pady=12, relief="flat", anchor="center", font=('Ivy 10 bold'), bg=co1, fg=co0)
+l_resultado_texto.place(x=0, y=85)
+
+# ------------ Botao calcular ------------------
+ 
+b_calcular = Button(frame_baixo, text="Calcular",width=34, height=1, overrelief=SOLID,  bg=co2, fg="white", font=('Ivy 10 bold'), anchor="center", relief=RAISED )
+# b_gerar_senha.grid(row=4, column=0,  sticky=NSEW, pady=60, padx=5, columnspan=30)
+ 
+
 janela.mainloop()
