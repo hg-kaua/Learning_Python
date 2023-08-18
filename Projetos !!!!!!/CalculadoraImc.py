8ec7d4ce0d512799b61462666039fb0295e1a231
Hugo Kaua <hugokaua2002@gmail.com>
2023-08-17 21:30:47 -0300

    Update CalculadoraImc.py
    finalizado

--- Projetos !!!!!!/CalculadoraImc.py
+++ Projetos !!!!!!/CalculadoraImc.py
@@ -5,7 +5,29 @@ from tkinter import ttk
 # construindo do zero calculadora de IMC
 
 def calcular_imc():
-    pass
+    # Abaixo do peso = menos de 18,5
+    # Normal = maior ou igual a 18,5 e menor que 25
+    # Sobrepeso = maior ou igual a 25 e menor que 30
+    # Obesidade = 30 ou mais
+    peso = float(entrada_peso.get())
+    altura = float(entrada_altura.get())**2
+    
+    imc = peso / altura
+    print(imc)
+    
+    if imc < 18.5:
+        resultado = f"Seu IMC é igual {imc:.2f} - Abaixo do peso"
+    elif imc >= 18.5 and imc < 25:
+        resultado = f"Seu IMC é igual {imc:.2f} - Normal"
+    elif imc >= 25 and imc < 30:
+        resultado = f"Seu IMC é igual {imc:.2f} - Sobrepeso"
+    elif imc >= 30:
+        resultado = f"Seu IMC é igual {imc:.2f} - Obesidade"
+    else:
+        resultado = f"Não foi possível calcular seu IMC"
+    
+    texto_resultado["text"] = resultado
+        
 
 # construindo interface grafica
 janela = Tk()
@@ -27,5 +49,8 @@ entrada_altura.grid(column=1,row=1)
 # botao calcular
 botao_calcular = Button(janela,text= "Calcular IMC", width=10, height=2, command=calcular_imc)
 botao_calcular.grid(column=0, row=3)
+
+texto_resultado = Label(janela, text="",relief="flat", anchor="center")
+texto_resultado.grid(column=5, row=3)
 
 janela.mainloop()
