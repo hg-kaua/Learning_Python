ea86213ff34fdd00d59db539c8afd725e6d69273
Hugo Kaua <hugokaua2002@gmail.com>
2023-08-29 21:04:16 -0300

    Update gerenciamento_veiculos.py

--- oo/gerenciamento_veiculos.py
+++ oo/gerenciamento_veiculos.py
@@ -1,20 +1,32 @@
 class Veiculo:
     
-    def __init__(self, categoria):
-        self.categoria = categoria
-        self.marca = None
-        self.ano = None
-        self.preco = None
+    def __init__(self, marca, modelo, ano, preco):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.preco = preco
+    
+    def calcular_custo_manun(self):
+        return 100 + (2023 - self.ano) * 30
     
     def __str__(self):
-        return f"""Categoria: {self.categoria}:
-    - Marca: {self.marca}
+        return f"""
+    - Marca: {self.marca}:
+    - Modelo: {self.modelo}
     - Ano: {self.ano}
     - Preco: {self.preco}"""
 
-carro = Veiculo("Carro")
-carro.ano = 1940
-carro.marca = "chevrolet"
-carro.preco = 200.000
+class Carro:
+    def __init__(self, marca, modelo, ano, preco, categoria = "Carro", vel_maxima = 150):
+        super().__init__(marca, modelo, ano, preco)
+        self.categoria = categoria
+        self.vel_maxima = vel_maxima
+    
+    def calcular_custo_manun(self):
+        super().calcular_custo_manun()
+    
+    def __str__(self):
+        super().__str__()
 
-print(carro)
+ferrari = Carro("ferrari", "ferrari", 2020, 1000000, vel_maxima=240)
+print(ferrari)
