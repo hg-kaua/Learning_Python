332366ac2944d66d350293d014ebbeb72cbaa5cc
Hugo Kaua <hugokaua2002@gmail.com>
2023-09-05 14:07:19 -0300

    commit

--- Projetos /pyautogui/rastreamentoEntrega/rastreamento_entrega.py
+++ Projetos /pyautogui/rastreamentoEntrega/rastreamento_entrega.py
@@ -1,9 +1,27 @@
 import pyautogui as pa
+import time
 import pandas as pd
 
+# abrindo pagina de login
+
+pa.PAUSE = 1
+pa.keyDown("command")
+pa.press("space")
+pa.keyUp("command")
+pa.typewrite("Login.xlsx")
+pa.press("enter")
+pa.hotkey("ctrl","command", "f")
+
+# fazendo o login
+
+pa.click(x=420, y=229)
+pa.typewrite("hugomedeiros")
+pa.click(x=393, y=262)
+pa.typewrite("Hugo@2323")
+pa.click(x=306, y=356)
+
+# inserir dados
+
+# ler os dados
 data_frame = pd.read_csv("info_pacote.csv")
-
-colunas = data_frame.columns
-
-print(colunas)
-
+data_frame["numero"]
--- Projetos /pyautogui/teste.py
+++ Projetos /pyautogui/teste.py
@@ -1,4 +1,5 @@
 import pyautogui as pa
+import time
+
+time.sleep(3)
 print(pa.position())
-pa.mouseDown(1374, 68, button='left', duration= 2)
-pa.dragTo( 1208,872,button='left', duration=2)
