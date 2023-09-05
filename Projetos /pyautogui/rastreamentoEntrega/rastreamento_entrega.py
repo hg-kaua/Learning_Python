56a320577d68d31b564b996a924199e52f93ffa9
Hugo Kaua <hugokaua2002@gmail.com>
2023-09-05 15:42:46 -0300

    commit

--- Projetos /pyautogui/rastreamentoEntrega/rastreamento_entrega.py
+++ Projetos /pyautogui/rastreamentoEntrega/rastreamento_entrega.py
@@ -1,6 +1,7 @@
 import pyautogui as pa
 import time
 import pandas as pd
+import openpyxl
 
 # abrindo pagina de login
 
@@ -10,10 +11,14 @@ pa.press("space")
 pa.keyUp("command")
 pa.typewrite("Login.xlsx")
 pa.press("enter")
+time.sleep(2)
 pa.hotkey("ctrl","command", "f")
+pa.click(x=97, y=862)
+time.sleep(1)
 
 # fazendo o login
 
+time.sleep(1)
 pa.click(x=420, y=229)
 pa.typewrite("hugomedeiros")
 pa.click(x=393, y=262)
@@ -24,4 +29,24 @@ pa.click(x=306, y=356)
 
 # ler os dados
 data_frame = pd.read_csv("info_pacote.csv")
-data_frame["numero"]
+i = 0
+time.sleep(1)
+for linha in data_frame.values:
+    numero_rastre = data_frame["Número de Rastreamento"][i]
+    pa.click(x=311, y=347, duration=1)
+    time.sleep(1)
+    pa.write(numero_rastre)
+    nome_dest = data_frame["Nome do Destinatário"][i]
+    pa.click(x=429, y=348, duration=1)
+    time.sleep(1)
+    pa.write(nome_dest)
+    data_entrega = data_frame["Data de Entrega"][i]
+    pa.click(x=580, y=345, duration=1)
+    time.sleep(1)
+    pa.write(data_entrega)
+    categoria_prod = data_frame["Categoria do Produto"][i]
+    pa.click(x=709, y=343, duration=1)
+    time.sleep(1)
+    pa.write(categoria_prod)
+    i+=1
+
