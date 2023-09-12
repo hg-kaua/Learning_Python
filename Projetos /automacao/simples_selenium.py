4f8c86b39ae4b22c30f332cd02df3489277eadbf
Hugo Kaua <hugokaua2002@gmail.com>
2023-09-11 22:15:53 -0300

    commit

---
+++ Projetos /automacao/simples_selenium.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+# Crie o driver do Selenium com as opções configuradas
+navegador = webdriver.Chrome(options=options)
+navegador.get("http://www.python.org")
+
+element = navegador.find_element("xpath", '//*[@id="documentation"]/a')
+element.click()
