9584f75af09b7209cfff3e4c6c26c4c969aac672
Hugo Kaua <hugokaua2002@gmail.com>
2023-09-17 21:27:54 -0300

    Update mac_otimizado.py

--- Projetos /pyautogui/mac-otimizado/mac_otimizado.py
+++ Projetos /pyautogui/mac-otimizado/mac_otimizado.py
@@ -1,95 +1,87 @@
 import pyautogui as pa
 import time
 
-# from mouseinfo import mouseInfo
+from mouseinfo import mouseInfo
 
-# mouseInfo()
+mouseInfo()
 
 # abrindo o apple music
+
+time.sleep(2)
 pa.keyDown("command")
 pa.press("space")
-pa.keyUp("command") 
+pa.keyUp("command")
+time.sleep(1)
+pa.write("musica")
+pa.press("enter")
+time.sleep(3)
+pa.click(172,141, duration=2)
+pa.hotkey("command", "backspace")
+pa.write("os planos de deus")
+pa.press("enter")
+pa.click(325,227, duration=2)
+
+
+# abrindo o chrome
+time.sleep(3)
+pa.keyDown("command")
+pa.press("space")
+pa.keyUp("command")
+time.sleep(1)
 pa.write("chrome")
 pa.press("enter")
+time.sleep(1)
+
+time.sleep(3)
+# abrindo sites diários
+# Email
+pa.write("https://mail.google.com/mail/u/0/#inbox")
+pa.press("enter")
+pa.hotkey("command","t")
 time.sleep(2)
-pa.click(pa.locateOnScreen("img.png"))
+# ChatGpt
+pa.write("https://chat.openai.com/c/5d76b576-50da-4216-8386-1d3d401fb481")
+pa.press("enter")
+pa.hotkey("command", "t")
+time.sleep(2)
+# Youtube
+pa.write("https://www.youtube.com/")
+pa.press("enter")
+pa.hotkey("command", "t")
+time.sleep(2)
+# Whatsapp
+pa.write("https://web.whatsapp.com/")
+pa.press("enter")
+time.sleep(2)
+# maxima a tela
+pa.hotkey("ctrl","command", "f")
 
+time.sleep(2)
+# abrindo o vscode
+pa.keyDown("command")
+pa.press("space")
+pa.keyUp("command")
+time.sleep(1)
+pa.write("visual studio code")
+pa.press("enter")
+time.sleep(2)
+pa.hotkey("shift","command", "n")
+time.sleep(2)
 
-# time.sleep(2)
-# pa.keyDown("command")
-# pa.press("space")
-# pa.keyUp("command")
-# time.sleep(1)
-# pa.write("musica")
-# pa.press("enter")
-# time.sleep(3)
-# pa.click(172,141, duration=2)
-# pa.hotkey("command", "backspace")
-# pa.write("os planos de deus")
-# pa.press("enter")
-# pa.click(325,227, duration=2)
+# abrindo pasta 
+pa.hotkey("command", "o")
+time.sleep(2)
+pa.click(973, 255, duration=2)
+pa.write("Learning_Python")
+pa.click(591,362, duration=2)
+pa.click(1028,643, duration=2)
 
-
-# # abrindo o chrome
-# time.sleep(3)
-# pa.keyDown("command")
-# pa.press("space")
-# pa.keyUp("command")
-# time.sleep(1)
-# pa.write("chrome")
-# pa.press("enter")
-# time.sleep(1)
-
-# time.sleep(3)
-# # abrindo sites diários
-# # Email
-# pa.write("https://mail.google.com/mail/u/0/#inbox")
-# pa.press("enter")
-# pa.hotkey("command","t")
-# time.sleep(2)
-# # ChatGpt
-# pa.write("https://chat.openai.com/c/5d76b576-50da-4216-8386-1d3d401fb481")
-# pa.press("enter")
-# pa.hotkey("command", "t")
-# time.sleep(2)
-# # Youtube
-# pa.write("https://www.youtube.com/")
-# pa.press("enter")
-# pa.hotkey("command", "t")
-# time.sleep(2)
-# # Whatsapp
-# pa.write("https://web.whatsapp.com/")
-# pa.press("enter")
-# time.sleep(2)
-# # maxima a tela
-# pa.hotkey("ctrl","command", "f")
-
-# time.sleep(2)
-# # abrindo o vscode
-# pa.keyDown("command")
-# pa.press("space")
-# pa.keyUp("command")
-# time.sleep(1)
-# pa.write("visual studio code")
-# pa.press("enter")
-# time.sleep(2)
-# pa.hotkey("shift","command", "n")
-# time.sleep(2)
-
-# # abrindo pasta 
-# pa.hotkey("command", "o")
-# time.sleep(2)
-# pa.click(973, 255, duration=2)
-# pa.write("Learning_Python")
-# pa.click(591,362, duration=2)
-# pa.click(1028,643, duration=2)
-
-# # abrindo o notion
-# time.sleep(2)
-# pa.keyDown("command")
-# pa.press("space")
-# pa.keyUp("command")
-# time.sleep(1)
-# pa.write("notion")
-# pa.press("enter")
+# abrindo o notion
+time.sleep(2)
+pa.keyDown("command")
+pa.press("space")
+pa.keyUp("command")
+time.sleep(1)
+pa.write("notion")
+pa.press("enter")
 
