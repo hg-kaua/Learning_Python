35179dc2f6016ae7edaf9c94c48d77ea83b84b7f
Hugo Kaua <hugokaua2002@gmail.com>
2023-09-24 10:03:49 -0300

    uptade

--- Projetos /automacao/top_investimentos.py
+++ Projetos /automacao/top_investimentos.py
@@ -4,6 +4,11 @@ from time import sleep
 import pandas as pd
 
 
+
+
+tempo = 0.5
+lista_parametros = ['800', '1000']
+
 options = webdriver.ChromeOptions()
 # options.add_argument('-headless')
 options.add_experimental_option("detach", True)
@@ -11,6 +16,8 @@ options.add_experimental_option("detach", True)
 url_base = 'https://statusinvest.com.br/'
 
 opcao = input("O que deseja procurar (1 - acao/ 2 -fundos imobiliarios)? ")
+
+
 
 def selecionar_investimento():  
     return 'acoes/busca-avancada' if opcao == '1' else 'fundos-imobiliarios/busca-avancada'
@@ -21,8 +28,8 @@ def analisar_fii():
     dy_min = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[2]/div[1]/div/div[2]/div[1]/input')
     dy_max = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[2]/div[1]/div/div[2]/div[2]/input')
     
-    sleep(0.5)
-    dy_min.send_keys('800')
+    sleep(tempo)
+    dy_min.send_keys(lista_parametros[0])
     dy_max.clear()
     dy_max.send_keys('1400')
     
@@ -30,16 +37,16 @@ def analisar_fii():
     p_vp_min = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[2]/div[2]/div/div[2]/div[1]/input')
     p_vp_max = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[2]/div[2]/div/div[2]/div[2]/input')
 
-    sleep(0.5)
+    sleep(tempo)
     p_vp_min.send_keys('080')
     p_vp_max.clear()
     p_vp_max.send_keys('150')
     
-    sleep(0.5)
+    sleep(tempo)
     num_cotista = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[2]/div[4]/div/div[2]/div[1]/input')
     num_cotista.send_keys('6500000')
     
-    sleep(0.5)
+    sleep(tempo)
     
     cagr = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[2]/div[5]/div/div[2]/div[1]/input')
     cagr.send_keys('500')
@@ -55,8 +62,8 @@ def analisar_acao():
     dy_min = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[4]/div[1]/div/div[2]/div[1]/input')
     dy_max = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[4]/div[1]/div/div[2]/div[2]/input')
     
-    sleep(0.5)
-    dy_min.send_keys('800')
+    sleep(tempo)
+    dy_min.send_keys(lista_parametros[0])
     dy_max.clear()
     dy_max.send_keys('1400')
     
@@ -64,18 +71,18 @@ def analisar_acao():
     p_vp_min = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[4]/div[4]/div/div[2]/div[1]/input')
     p_vp_max = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[4]/div[4]/div/div[2]/div[2]/input')
 
-    sleep(0.5)
+    sleep(tempo)
     p_vp_min.send_keys('80')
     p_vp_max.clear()
     p_vp_max.send_keys('150')
     
     # inserindo margem liquida
     margem_liquida_min = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[4]/div[8]/div/div[2]/div[1]/input')
-    margem_liquida_min.send_keys('1000')
+    margem_liquida_min.send_keys(lista_parametros[1])
     
     # inserindo o roe
     roe = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/form/div[4]/div[16]/div/div[2]/div[1]/input')
-    roe.send_keys('1000')
+    roe.send_keys(lista_parametros[1])
     
     search = nav.find_element('xpath', '//*[@id="main-2"]/div[3]/div/div/div/button[2]')
     search.click()
