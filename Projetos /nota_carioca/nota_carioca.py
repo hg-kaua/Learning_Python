43cedb53bef17240c2105c7534711f7f184fd385
Hugo Kaua <hugokaua2002@gmail.com>
2023-10-10 22:44:58 -0300

    commit

---
+++ Projetos /nota_carioca/nota_carioca.py
@@ -0,0 +1,30 @@
+
+from selenium import webdriver
+import pyautogui as pa
+from selenium.webdriver.edge.service import Service as EdgeService
+
+
+url_icp = 'https://notacarioca.rio.gov.br/senhaweb/loginICP.aspx'
+
+# pegar o certificado icp brasil (pendencia)
+
+# fazer login com certificado
+
+# clicar em acessar o sistema
+
+# clicar na aba consulta de notas
+
+# clicar na aba notas fiscais
+
+# clicar em notas recebidas
+
+# competencias
+    #  pegar data atual que o robo esta rodando e selecionar o mes
+
+# clicar em consulta
+
+# segunda linha da segunda tabela transforma o XML em PDF
+
+#  clicar em exportar
+
+# esperar o pdf carregar
